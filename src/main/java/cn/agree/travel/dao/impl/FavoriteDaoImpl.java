963772a1aaf134468961821e7c4e9f35658d5381
zhangxianlin <zhangxianlin2431@163.com>
2022-04-02 23:40:45 +0800

    完成用户已经登录的情况下 看这条线路是否被该用户收藏,以便前端判断 "收藏按钮"是否可以被点击

--- src/main/java/cn/agree/travel/dao/impl/FavoriteDaoImpl.java
+++ src/main/java/cn/agree/travel/dao/impl/FavoriteDaoImpl.java
@@ -4,6 +4,8 @@ import cn.agree.travel.dao.IFavoriteDao;
 import cn.agree.travel.model.Favorite;
 import cn.agree.travel.model.User;
 import cn.agree.travel.util.JDBCUtil;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class FavoriteDaoImpl implements IFavoriteDao {
@@ -12,7 +14,14 @@ public class FavoriteDaoImpl implements IFavoriteDao {
 
     @Override
     public Favorite findFavorite(String rid, User user) {
-        return null;
+        String sql = "select * from tab_favorite where rid=? and uid=?";
+        Favorite favorite = null;
+        try {
+            favorite = template.queryForObject(sql, new BeanPropertyRowMapper<>(Favorite.class), rid, user.getUid());
+        } catch (EmptyResultDataAccessException e) {
+            e.printStackTrace();
+        }
+        return favorite;
     }
 
     @Override
--- src/main/java/cn/agree/travel/service/impl/FavoriteServiceImpl.java
+++ src/main/java/cn/agree/travel/service/impl/FavoriteServiceImpl.java
@@ -1,17 +1,24 @@
 package cn.agree.travel.service.impl;
 
+import cn.agree.travel.dao.IFavoriteDao;
+import cn.agree.travel.dao.impl.FavoriteDaoImpl;
 import cn.agree.travel.model.Favorite;
 import cn.agree.travel.model.User;
 import cn.agree.travel.service.IFavoriteService;
 
 public class FavoriteServiceImpl implements IFavoriteService {
+
+    private IFavoriteDao favoriteDao = new FavoriteDaoImpl();
+
     @Override
     public Favorite isFavorite(String rid, User user) {
-        return null;
+        Favorite favorite = favoriteDao.findFavorite(rid, user);
+        return favorite;
     }
 
     @Override
     public boolean addFavorite(User user, String rid) {
+
         return false;
     }
 
